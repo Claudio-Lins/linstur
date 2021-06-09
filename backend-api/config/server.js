442328df3289709f8d33345706c6c6144402598e
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-09 21:55:23 +0100

    init

---
+++ backend-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8a0403b423812ce9a0d40ec343597671'),
+    },
+  },
+});
---
+++ backend-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ba6aa7f9-992a-41f4-9851-6e2588408a6b'
+};
